2371e475f13a60f1b5dc34f61788677b76a35245
KeyurVastani <vastanikeyur1711@gmail.com>
2021-07-12 15:36:05 +0530

    this is a first time

---
+++ 6ControlStat.js
@@ -0,0 +1,49 @@
+// // if else statement is
+
+// x=6
+// if (x>0){
+//     console.log("x is bigger");
+// }else{
+//     console.log("xis smaller");
+// }
+
+//------------------------------------------------------
+//leap  year is leap year or not in leap year 365
+
+// var x=2000
+// // debugger; advance concept
+// if(x%4===0){
+//     if(x%100===0){ 
+//         if(x%400===0){
+//              console.log("this is  a leap year");
+//         }else{
+//             console.log("this is not a leap year");
+//         }
+
+//     }else{
+//         console.log("this is a leap year");
+//     }
+// }else{
+//     console.log("this is not a leap year");
+// }
+ 
+// var year=2020
+// if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+//     console.log(year + ' is a leap year');
+// } else {
+//     console.log(year + ' is not a leap year');
+// }
+
+
+//------------------------------------------------------
+//falsy value in javascript
+// 0,"",undefined, null, NaN
+
+//if(score=0)
+// if(NaN){
+//     console.log("we won the game");
+
+// }else{
+//     console.log("we loss the game");
+// }
+ 
